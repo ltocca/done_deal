5bacbb1977113ee4c24d01fd823af19c3960d13e
Leonardo Toccafondi <leonardo.toccafondi1@stud.unifi.it>
2023-06-16 23:19:15 +0200

    upd settings.py

--- done_deal/settings.py
+++ done_deal/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
